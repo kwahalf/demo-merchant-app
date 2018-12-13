bae3d53267cf271ee2d4b3ecffa5d5e4c6ec8eb4
kwahalf <kwanusud@gmail.com>
2018-12-13 15:16:18 +0300

    minor improvement

--- server.js
+++ server.js
@@ -14,11 +14,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // create our webhook endpoint to recive webhooks from Safaricom
-app.post('/hooks/mpesa', (req, res) => {
+app.post('/hooks/payments', (req, res) => {
   console.log('-----------Received M-Pesa webhook-----------');
   
   // format and dump the request payload recieved from safaricom in the terminal
   console.log(prettyjson.render(req.body, options));
+  console.log('-----------------------');
+  
+  let message = {
+    "ResponseCode": "00000000",
+    "ResponseDesc": "success"
+  };
+  
+  // respond to safaricom servers with a success message
+  res.json(message);
+});
+
+app.get('/hooks/validate', (req, res) => {
+  console.log('-----------Received M-Pesa webhook-----------');
+  
+  // format and dump the request payload recieved from safaricom in the terminal
+  console.log(prettyjson.render(req.params, options));
   console.log('-----------------------');
   
   let message = {
